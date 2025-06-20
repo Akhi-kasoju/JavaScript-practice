9b56fd25a74b3fd3f52558b2c8187d6fd792c163
Akhi-kasoju <kasojusonu226@gmail.com>
2025-06-20 12:54:19 +0530

    Replaced intersection.js with updated code

--- intersection.js
+++ intersection.js
@@ -1,26 +1,12 @@
-function intersection(arr1, arr2) {
-    const set1 = new Set(arr1);
-    const result = new Set();
-
-    for(let num of arr2) {
-        if(set1.has(num)) result.add(num);
-    }
-
-    return [...result];
-}
-
-console.log(intersection([1,2,2,3], [2,3,4])); 
-
-
-function intersection(arr1,arr2) {
+function intersection(arr1,arr2){
     const set1 = new Set(arr1)
-    const result =new Set()
+    const result = new Set()
 
     for(let num of arr2){
         if(set1.has(num)) result.add(num)
     }
 
-    return [...result]
+    return[result]
 }
 
-console.log(intersection([1,2,4,5,] , [5,7,8,2]))
+console.log(intersection([1,2,3,4,] , [4,7,2,5]))
