27cdd6c7a63ec3b5d813746df8ffe5bee7e7d9cf
Akhi-kasoju <kasojusonu226@gmail.com>
2025-06-20 11:38:30 +0530

    Added single file

---
+++ missingNumber.js
@@ -0,0 +1,7 @@
+function findMissingNumber(arr, n) {
+    let expectedSum = (n * (n + 1)) / 2;
+    let actualSum = arr.reduce((sum, num) => sum + num, 0);
+    return expectedSum - actualSum;
+}
+
+console.log(findMissingNumber([1,2,3,5,6], 6)); 
